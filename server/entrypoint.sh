3af24ec9cac0020a9e469dab339ef8c81a0f5dda
Aelita-S <1225438527@qq.com>
2020-08-09 06:02:02 +0800

    调整worker数量

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 rm -rf /judger/*
 mkdir -p /judger/run /judger/spj
 
@@ -8,6 +9,14 @@ chmod 711 /judger/run
 chown compiler:spj /judger/spj
 chmod 710 /judger/spj
 
-core=$(grep --count ^processor /proc/cpuinfo)
-n=$(($core*2))
-exec gunicorn --workers $n --threads $n --error-logfile /log/gunicorn.log --time 600 --bind 0.0.0.0:8080 server:app
+if [ -z "$MAX_WORKER_NUM" ]; then
+  CPU_CORE_NUM=$(grep -c ^processor /proc/cpuinfo)
+  export CPU_CORE_NUM
+  if [ "$CPU_CORE_NUM" -lt 2 ]; then
+    export MAX_WORKER_NUM=2
+  else
+    export MAX_WORKER_NUM=$((CPU_CORE_NUM / 2))
+  fi
+fi
+
+exec gunicorn --workers "$MAX_WORKER_NUM" --threads "$MAX_WORKER_NUM" --error-logfile /log/gunicorn.log --time 600 --bind 0.0.0.0:8080 server:app
