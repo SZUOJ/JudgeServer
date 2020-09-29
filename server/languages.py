241fc9f347f2cafddc6e2065040e007df5c44bf0
Aelita-S <1225438527@qq.com>
2020-09-29 19:32:51 +0800

    update: languages.py

--- server/languages.py
+++ server/languages.py
@@ -1,6 +1,8 @@
 # coding=utf-8
 from __future__ import unicode_literals
 
+from utils import ProblemIOMode
+
 default_env = ["LANG=en_US.UTF-8", "LANGUAGE=en_US:en", "LC_ALL=en_US.UTF-8"]
 
 
@@ -9,13 +11,13 @@ c_lang_config = {
         "src_name": "main.c",
         "exe_name": "main",
         "max_cpu_time": 3000,
-        "max_real_time": 5000,
-        "max_memory": 128 * 1024 * 1024,
-        "compile_command": "/usr/bin/gcc -DONLINE_JUDGE -O2 -w -fmax-errors=3 -std=c99 {src_path} -lm -o {exe_path}",
+        "max_real_time": 10000,
+        "max_memory": 256 * 1024 * 1024,
+        "compile_command": "/usr/bin/gcc -DONLINE_JUDGE -O2 -w -fmax-errors=3 -std=c11 {src_path} -lm -o {exe_path}",
     },
     "run": {
         "command": "{exe_path}",
-        "seccomp_rule": "c_cpp",
+        "seccomp_rule": {ProblemIOMode.standard: "c_cpp", ProblemIOMode.file: "c_cpp_file_io"},
         "env": default_env
     }
 }
@@ -39,16 +41,31 @@ cpp_lang_config = {
     "compile": {
         "src_name": "main.cpp",
         "exe_name": "main",
-        "max_cpu_time": 3000,
-        "max_real_time": 5000,
-        "max_memory": 128 * 1024 * 1024,
-        "compile_command": "/usr/bin/g++ -DONLINE_JUDGE -O2 -w -fmax-errors=3 -std=c++11 {src_path} -lm -o {exe_path}",
+        "max_cpu_time": 10000,
+        "max_real_time": 20000,
+        "max_memory": 1024 * 1024 * 1024,
+        "compile_command": "/usr/bin/g++ -DONLINE_JUDGE -O2 -w -fmax-errors=3 -std=c++14 {src_path} -lm -o {exe_path}",
     },
     "run": {
         "command": "{exe_path}",
-        "seccomp_rule": "c_cpp",
+        "seccomp_rule": {ProblemIOMode.standard: "c_cpp", ProblemIOMode.file: "c_cpp_file_io"},
         "env": default_env
     }
+}
+
+cpp_lang_spj_compile = {
+    "src_name": "spj-{spj_version}.cpp",
+    "exe_name": "spj-{spj_version}",
+    "max_cpu_time": 10000,
+    "max_real_time": 20000,
+    "max_memory": 1024 * 1024 * 1024,
+    "compile_command": "/usr/bin/g++ -DONLINE_JUDGE -O2 -w -fmax-errors=3 -std=c++14 {src_path} -lm -o {exe_path}"
+}
+
+cpp_lang_spj_config = {
+    "exe_name": "spj-{spj_version}",
+    "command": "{exe_path} {in_file_path} {user_out_file_path}",
+    "seccomp_rule": "c_cpp"
 }
 
 java_lang_config = {
@@ -56,13 +73,14 @@ java_lang_config = {
     "compile": {
         "src_name": "Main.java",
         "exe_name": "Main",
-        "max_cpu_time": 3000,
-        "max_real_time": 5000,
+        "max_cpu_time": 5000,
+        "max_real_time": 10000,
         "max_memory": -1,
         "compile_command": "/usr/bin/javac {src_path} -d {exe_dir} -encoding UTF8"
     },
     "run": {
-        "command": "/usr/bin/java -cp {exe_dir} -XX:MaxRAM={max_memory}k -Djava.security.manager -Dfile.encoding=UTF-8 -Djava.security.policy==/etc/java_policy -Djava.awt.headless=true Main",
+        "command": "/usr/bin/java -cp {exe_dir} -XX:MaxRAM={max_memory}k -Djava.security.manager -Dfile.encoding=UTF-8 "
+                   "-Djava.security.policy==/etc/java_policy -Djava.awt.headless=true Main",
         "seccomp_rule": None,
         "env": default_env,
         "memory_limit_check_only": 1
@@ -75,7 +93,7 @@ py2_lang_config = {
         "src_name": "solution.py",
         "exe_name": "solution.pyc",
         "max_cpu_time": 3000,
-        "max_real_time": 5000,
+        "max_real_time": 10000,
         "max_memory": 128 * 1024 * 1024,
         "compile_command": "/usr/bin/python -m py_compile {src_path}",
     },
@@ -91,14 +109,14 @@ py3_lang_config = {
         "src_name": "solution.py",
         "exe_name": "__pycache__/solution.cpython-36.pyc",
         "max_cpu_time": 3000,
-        "max_real_time": 5000,
+        "max_real_time": 10000,
         "max_memory": 128 * 1024 * 1024,
         "compile_command": "/usr/bin/python3 -m py_compile {src_path}",
     },
     "run": {
         "command": "/usr/bin/python3 {exe_path}",
         "seccomp_rule": "general",
-        "env": ["PYTHONIOENCODING=UTF-8"] + default_env
+        "env": default_env + ["PYTHONIOENCODING=utf-8"]
     }
 }
 
@@ -116,7 +134,7 @@ go_lang_config = {
         "command": "{exe_path}",
         "seccomp_rule": "",
         # 降低内存占用
-        "env": ["GODEBUG=madvdontneed=1", "GOCACHE=off"] + default_env,
+        "env": ["GODEBUG=madvdontneed=1"] + default_env,
         "memory_limit_check_only": 1
     }
 }
