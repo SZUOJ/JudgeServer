be254496ca78c6f6c3cbe9194c2680937fb72000
Aelita-S <1225438527@qq.com>
2020-12-07 18:17:52 +0800

    feat: 优化编译错误状态码(400)

--- server/exception.py
+++ server/exception.py
@@ -1,10 +1,18 @@
 class JudgeServerException(Exception):
+    status = 500
+
     def __init__(self, message):
         super().__init__()
         self.message = message
 
 
 class CompileError(JudgeServerException):
+    """编译错误"""
+    status = 400
+
+
+class CompilerRuntimeError(JudgeServerException):
+    """编译器运行异常"""
     pass
 
 
