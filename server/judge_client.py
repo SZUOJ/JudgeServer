90620c71ff136dc3f8e733da0f9f4fc3a962822d
DongYunhao <2274006799@qq.com>
2026-04-16 00:45:43 +0800

    fix

--- server/judge_client.py
+++ server/judge_client.py
@@ -54,7 +54,7 @@ class JudgeClient(object):
         self._exe_path = exe_path
         self._max_cpu_time = max_cpu_time
         self._max_memory = max_memory
-        self._max_real_time = self.max_real_time
+        self._max_real_time = max_real_time
         self._test_case_dir = test_case_dir
         self._submission_dir = submission_dir
 
